ba5781a6e27995277a0df7b1d0bb82667e50be4c
aa1234241 <bai.aa1234241@gmail.com>
2024-01-03 14:24:34 +0800

    add automix precision training

--- training_vqgan.py
+++ training_vqgan.py
@@ -21,10 +21,10 @@ class TrainVQGAN:
     def __init__(self, args):
         self.vqgan = VQGAN(args).to(device=args.device)
         self.vqgan.load_state_dict(
-            torch.load(os.path.join("/media/userdisk1/code/VQGAN-pytorch/checkpoints", "vqgan_epoch_306.pt")))
+            torch.load(os.path.join("/media/userdisk1/code/VQGAN-pytorch/checkpoints", "vqgan_epoch_308.pt")))
         self.discriminator = Discriminator(args).to(device=args.device)
         self.discriminator.load_state_dict(
-            torch.load(os.path.join("/media/userdisk1/code/VQGAN-pytorch/checkpoints", "discriminator_epoch_306.pt")))
+            torch.load(os.path.join("/media/userdisk1/code/VQGAN-pytorch/checkpoints", "discriminator_epoch_308.pt")))
 
         # self.discriminator.apply(weights_init)
         self.perceptual_loss = LPIPS().eval().to(device=args.device)
@@ -58,46 +58,55 @@ class TrainVQGAN:
     def train(self, args):
         train_dataset = load_data(args)
         steps_per_epoch = len(train_dataset)
-        for epoch in range(173, args.epochs):
+        scaler = torch.cuda.amp.GradScaler()
+        for epoch in range(309, args.epochs):
             usage_codebook = np.zeros(shape=(args.num_codebook_vectors))
             with tqdm(range(len(train_dataset))) as pbar:
                 for i, imgs in zip(pbar, train_dataset):
-                    imgs = imgs.to(device=args.device)
                     self.discriminator.zero_grad()
-                    with torch.no_grad():
-                        decoded_images, _, q_loss = self.vqgan(imgs)
-                    disc_real = self.discriminator(imgs)
-                    disc_fake = self.discriminator(decoded_images.detach())
-                    d_loss_real = torch.mean(F.relu(1. - disc_real))
-                    d_loss_fake = torch.mean(F.relu(1. + disc_fake))
-                    disc_factor = self.vqgan.adopt_weight(args.disc_factor, epoch * steps_per_epoch + i,
-                                                          threshold=args.disc_start)
-                    gan_loss = disc_factor * 0.5 * (d_loss_real + d_loss_fake)
                     self.opt_disc.zero_grad()
-                    gan_loss.backward()
+                    with autocast(device_type='cuda', dtype=torch.float16):
+                        imgs = imgs.to(device=args.device)
+
+                        with torch.no_grad():
+                            decoded_images, _, q_loss = self.vqgan(imgs)
+                        disc_real = self.discriminator(imgs)
+                        disc_fake = self.discriminator(decoded_images.detach())
+                        d_loss_real = torch.mean(F.relu(1. - disc_real))
+                        d_loss_fake = torch.mean(F.relu(1. + disc_fake))
+                        disc_factor = self.vqgan.adopt_weight(args.disc_factor, epoch * steps_per_epoch + i,
+                                                              threshold=args.disc_start)
+                        gan_loss = disc_factor * 0.5 * (d_loss_real + d_loss_fake)
+                    scaler.scale(gan_loss).backward()
+                    scaler.step(self.opt_disc)
+                    scaler.update()
+
+                    # gan_loss.backward()
                     # print(self.vqgan.decoder.conv_out.weight.grad.max(), self.discriminator.model[-1].weight.grad.max())
 
-                    self.opt_disc.step()
+                    # self.opt_disc.step()
 
                     self.vqgan.zero_grad()
-                    decoded_images, min_encoding_indices, q_loss = self.vqgan(imgs)
+                    self.opt_vq.zero_grad()
+                    with autocast(device_type='cuda', dtype=torch.float16):
+                        # imgs = imgs.to(device=args.device)
+                        decoded_images, min_encoding_indices, q_loss = self.vqgan(imgs)
+                        perceptual_loss = self.perceptual_loss(imgs.contiguous(), decoded_images.contiguous())
+                        rec_loss = torch.abs(imgs.contiguous() - decoded_images.contiguous())
+                        disc_fake = self.discriminator(decoded_images)
+                        perceptual_rec_loss = args.perceptual_loss_factor * perceptual_loss + args.rec_loss_factor * rec_loss
+                        perceptual_rec_loss = perceptual_rec_loss.mean()
+                        g_loss = -torch.mean(disc_fake)
+
+                        lbd = self.vqgan.calculate_lambda(perceptual_rec_loss, g_loss)
+                        vq_loss = perceptual_rec_loss + q_loss + disc_factor * lbd * g_loss
                     min_encoding_indices_int = min_encoding_indices.cpu()
                     for j in range(len(min_encoding_indices_int)):
                         usage_codebook[min_encoding_indices_int[j]] += 1
-                    perceptual_loss = self.perceptual_loss(imgs.contiguous(), decoded_images.contiguous())
-                    rec_loss = torch.abs(imgs.contiguous() - decoded_images.contiguous())
-                    disc_fake = self.discriminator(decoded_images)
-                    perceptual_rec_loss = args.perceptual_loss_factor * perceptual_loss + args.rec_loss_factor * rec_loss
-                    perceptual_rec_loss = perceptual_rec_loss.mean()
-                    g_loss = -torch.mean(disc_fake)
 
-                    lbd = self.vqgan.calculate_lambda(perceptual_rec_loss, g_loss)
-                    vq_loss = perceptual_rec_loss + q_loss + disc_factor * lbd * g_loss
-
-                    self.opt_vq.zero_grad()
-                    vq_loss.backward()
-                    # print(self.vqgan.decoder.conv_out.weight.grad.max(), self.discriminator.model[-1].weight.grad.max())
-                    self.opt_vq.step()
+                    scaler.scale(vq_loss).backward()
+                    scaler.step(self.opt_vq)
+                    scaler.update()
 
                     # self.opt_disc.zero_grad()
                     # gan_loss.backward()
